2ac1158bf8fcfbf63a8ebb286e38ee12e64a897f
Juliano Marcon <juliano.marcon@solagora.com.br>
2024-04-22 18:01:14 -0300

    Adding tmux config

--- Install/git_repos.zsh
+++ Install/git_repos.zsh
@@ -4,7 +4,14 @@
 if command -v git >/dev/null 2>&1; then
     if [ ! -d ~/.dotfiles/.fzf-git ]; then
         git clone https://github.com/junegunn/fzf-git.sh.git ~/.dotfiles/.fzf-git
+    fi
+
+    if [ ! -d ~/.git_repos/dotfiles ]; then
         git clone git@github.com:jmarcon/dotfiles.git ~/.git_repos/dotfiles
+    fi
+
+    if [ ! -d ~/.tmux/plugins/tpm ]; then
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     fi
 else
     echo "git is not installed"
