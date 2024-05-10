1804260fb45975352644c08b4829c5fd1886cc97
Juliano Marcon <1767705+jmarcon@users.noreply.github.com>
2024-05-10 15:54:11 -0300

    Update 2400_nodejs.zsh | Validate Brew

--- Config/zsh/init/2400_nodejs.zsh
+++ Config/zsh/init/2400_nodejs.zsh
@@ -4,8 +4,10 @@ if [[ "$DEBUG_DOTFILES" == "true" ]]; then
 fi
 
 # If nvm is installed via homebrew
-[ -s "$(brew --prefix nvm)/nvm.sh" ] && \. "$(brew --prefix nvm)/nvm.sh"  # This loads nvm
-[ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix nvm)/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+if command -v brew >/dev/null 2>&1; then
+    [ -s "$(brew --prefix nvm)/nvm.sh" ] && \. "$(brew --prefix nvm)/nvm.sh"  # This loads nvm
+    [ -s "$(brew --prefix nvm)/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix nvm)/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+fi 
 
 # NVM
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
