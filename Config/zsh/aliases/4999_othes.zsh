bd7e55ded64fb55f459988e20fc06ca224a5f6ab
Juliano Marcon <1767705+jmarcon@users.noreply.github.com>
2024-05-10 15:52:40 -0300

    Update 4999_othes.zsh | BatCat from apt-get

--- Config/zsh/aliases/4999_othes.zsh
+++ Config/zsh/aliases/4999_othes.zsh
@@ -10,8 +10,11 @@ alias bpytop="btop"
 fi 
 
 if command -v bat >/dev/null 2>&1; then
-# alias cat="bat --paging never --theme Dracula --tabs 2"
 alias cat="bat --paging never --theme DarkNeon --tabs 2"
+fi 
+
+if command -v batcat >/dev/null 2>&1; then
+alias cat="batcat --paging never --theme Dracula --tabs 2"
 fi 
 
 if command -v nvim >/dev/null 2>&1; then
