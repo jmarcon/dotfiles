d44d4920d58dd2b18ad247f34c51ccfca0bf48b2
Juliano Marcon <1767705+jmarcon@users.noreply.github.com>
2024-05-10 15:51:44 -0300

    Update 4100_file_listing.zsh to remove git from the command
    The ARM64 Version of eza doesn't have git integration

--- Config/zsh/aliases/4100_file_listing.zsh
+++ Config/zsh/aliases/4100_file_listing.zsh
@@ -4,19 +4,19 @@ if [[ "$DEBUG_DOTFILES" == "true" ]]; then
 fi
 
 if command -v eza >/dev/null 2>&1; then
-# alias ls='eza --icons=always --color=always --git-ignore --git --no-filesize --no-time --no-user --no-permissions --long'
-    alias ls='eza --icons=always --color=always --git-ignore --git'
-    alias ll='eza --icons=always --color=always --git-ignore --git --long --all'
-    alias la='eza --icons=always --color=always --git-ignore --git --all'
-    alias lc='eza --icons=always --color=always --git-ignore --git --long'
-    alias lh='eza --icons=always --color=always --git-ignore --git --long --all -h'
-    alias lg='eza --icons=always --color=always --git-ignore --git --long --all --group-directories-first'
-    alias lt='eza --icons=always --color=always --git-ignore --git --tree --level=2 --group-directories-first --all'
-    alias lt1='eza --icons=always --color=always --git-ignore --git --tree --level=1 --group-directories-first --all'
+# alias ls='eza --icons=always --color=always  --no-filesize --no-time --no-user --no-permissions --long'
+    alias ls='eza --icons=always --color=always '
+    alias ll='eza --icons=always --color=always  --long --all'
+    alias la='eza --icons=always --color=always  --all'
+    alias lc='eza --icons=always --color=always  --long'
+    alias lh='eza --icons=always --color=always  --long --all -h'
+    alias lg='eza --icons=always --color=always  --long --all --group-directories-first'
+    alias lt='eza --icons=always --color=always  --tree --level=2 --group-directories-first --all'
+    alias lt1='eza --icons=always --color=always  --tree --level=1 --group-directories-first --all'
     alias lt2=lt 
-    alias lt3='eza --icons=always --color=always --git-ignore --git --tree --level=3 --group-directories-first --all'
-    alias lt4='eza --icons=always --color=always --git-ignore --git --tree --level=4 --group-directories-first --all'
-    alias  l='eza --icons=always --color=always --git-ignore --git --no-filesize --no-time --no-user --no-permissions --long --tree --level=1 --group-directories-first --all'
+    alias lt3='eza --icons=always --color=always  --tree --level=3 --group-directories-first --all'
+    alias lt4='eza --icons=always --color=always  --tree --level=4 --group-directories-first --all'
+    alias  l='eza --icons=always --color=always  --no-filesize --no-time --no-user --no-permissions --long --tree --level=1 --group-directories-first --all'
 else
     alias ll="ls -lAh"
     alias la="ls -la"
