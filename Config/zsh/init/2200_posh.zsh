c9106636b7c0f2b07b0114b4e62d08601348f92d
Juliano Marcon <juliano.marcon@solagora.com.br>
2024-05-20 15:53:23 -0300

    Fix eval oh-my-posh init

--- Config/zsh/init/2200_posh.zsh
+++ Config/zsh/init/2200_posh.zsh
@@ -8,9 +8,9 @@ if command -v oh-my-posh >/dev/null 2>&1; then
         eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/iterm2.omp.json)"
     else
         if [ -n "${POSH_THEMES_PATH+1}" ]; then
-            eval $(oh-my-posh init zsh --config "$POSH_THEMES_PATH/iterm2.omp.json")
+            eval "$(oh-my-posh init zsh --config \"$POSH_THEMES_PATH/iterm2.omp.json\")"
         elif [ -d "/home/vscode/.oh-my-posh/themes" ]; then
-            eval $(oh-my-posh init zsh --config '/home/vscode/.oh-my-posh/themes/iterm2.omp.json')
+            eval "$(oh-my-posh init zsh --config '/home/vscode/.oh-my-posh/themes/iterm2.omp.json')"
         fi
     fi
-fi 
+fi
