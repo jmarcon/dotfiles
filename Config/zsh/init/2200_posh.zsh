f8c6c09306540999f1985576918a8e25fe7cde6a
Juliano Marcon <juliano.marcon@solagora.com.br>
2024-05-20 15:57:29 -0300

    set POST_THEME outside the init zsh

--- Config/zsh/init/2200_posh.zsh
+++ Config/zsh/init/2200_posh.zsh
@@ -8,9 +8,11 @@ if command -v oh-my-posh >/dev/null 2>&1; then
         eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/iterm2.omp.json)"
     else
         if [ -n "${POSH_THEMES_PATH+1}" ]; then
-            eval "$(oh-my-posh init zsh --config \"$POSH_THEMES_PATH/iterm2.omp.json\")"
+            eval "$(oh-my-posh init zsh)"
+            POSH_THEME="$POSH_THEMES_PATH/iterm2.omp.json"
         elif [ -d "/home/vscode/.oh-my-posh/themes" ]; then
-            eval "$(oh-my-posh init zsh --config '/home/vscode/.oh-my-posh/themes/iterm2.omp.json')"
+            eval "$(oh-my-posh init zsh)"
+            POSH_THEME='/home/vscode/.oh-my-posh/themes/iterm2.omp.json'
         fi
     fi
 fi
