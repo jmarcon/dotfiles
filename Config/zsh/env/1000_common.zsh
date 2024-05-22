c4212b110c052cfb61f4723943853e7d29c85395
Juliano Marcon <juliano.marcon@solagora.combr>
2024-05-22 12:08:53 -0300

    chore: Update Homebrew support for Linux in zsh configuration files

--- Config/zsh/env/1000_common.zsh
+++ Config/zsh/env/1000_common.zsh
@@ -2,6 +2,15 @@
 if [[ "$DEBUG_DOTFILES" == "true" ]]; then
     print -P '%F{yellow}  ♾️️ Loading Environment Variables [1000] - Common'
 fi
+
+# Homebrew (linux)
+# If the file brew exists, then load the shellenv : correct path
+
+if [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+
 # Using my birth year, just because
 export PORT=1982
 export APP_ENV=local
