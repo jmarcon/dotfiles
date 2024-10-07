329633e2fc607c845d0125ca389f151048600c4d
Juliano Marcon (CTO) <juliano.marcon@solagora.com.br>
2024-10-07 16:34:00 -0300

    LRCF

--- Install/os.zsh
+++ Install/os.zsh
@@ -1,20 +1,36 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 
 # What OS is this?
 # OS detection
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     export CURRENT_OS="linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    export CURRENT_OS="mac"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-    export CURRENT_OS="windows"
-elif [[ "$OSTYPE" == "msys" ]]; then
-    export CURRENT_OS="windows"
-elif [[ "$OSTYPE" == "win32" ]]; then
-    export CURRENT_OS="windows"
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    export CURRENT_OS="freebsd"
-else
-    export CURRENT_OS="unknown"
-    echo "Unknown OS: $OSTYPE"
+    return
 fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export CURRENT_OS="mac"
+    return
+fi
+
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    export CURRENT_OS="windows"
+    return
+fi
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    export CURRENT_OS="windows"
+    return
+fi
+
+if [[ "$OSTYPE" == "win32" ]]; then
+    export CURRENT_OS="windows"
+    return
+fi
+
+if [[ "$OSTYPE" == "freebsd"* ]]; then
+    export CURRENT_OS="freebsd"
+    return
+fi
+
+export CURRENT_OS="unknown"
+echo "Unknown OS: $OSTYPE"
