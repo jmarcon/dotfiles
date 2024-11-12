cdb4e96aaea33056dff38d6452162af22cd7297d
Juliano Marcon <juliano.marcon@solagora.com.br>
2024-11-12 13:09:39 -0300

    fix: Correct string interpolation and formatting in PowerShell profile module loading

--- Config/pwsh/profile/0200_modules.ps1
+++ Config/pwsh/profile/0200_modules.ps1
@@ -1,9 +1,9 @@
-if ($ENV:PROFILE_DEBUG -eq $true) {
-    Write-Host 'Loading Modules'
+if ($env:PROFILE_DEBUG -eq $true) {
+    Write-Host "Loading Modules"
 }
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path("$ChocolateyProfile")) {
     Import-Module "$ChocolateyProfile" -ErrorAction SilentlyContinue
 }
 
@@ -21,6 +21,6 @@ $modules_to_load = @(
 )
 
 foreach ($module in $modules_to_load) {
-    Import-Module -Name $module -ErrorAction SilentlyContinue
+    Import-Module -Name "$module" -ErrorAction SilentlyContinue
 }
 
