c355a70fcf990dbc7232878b49817c4211b9618b
Juliano Marcon <juliano.marcon@solagora.com.br>
2024-11-12 13:20:20 -0300

    feat: Enhance PowerShell aliases with additional eza options for improved file listing

--- Config/pwsh/profile/0500_aliases.ps1
+++ Config/pwsh/profile/0500_aliases.ps1
@@ -42,14 +42,45 @@ if (Get-Command "eza" -ErrorAction SilentlyContinue) {
     Remove-Alias -Name la -Force -ErrorAction SilentlyContinue
     Remove-Alias -Name ll -Force -ErrorAction SilentlyContinue
 
+    $ic = @("--icons=always", "--color=always", "--no-time")
+    $la = @("--long", "--all")
+    $tr = @("--tree", "--group-directories-first", "--all")
+
     function ls {
-        eza --no-time $args
+        eza $ic $args
     }
     function la {
-        eza --all --no-time $args
+        eza $ic --all $args
     }
     function ll {
-        eza --long --all --no-time $args
+        eza $ic $la $args
+    }
+    function lc {
+        eza $ic --long $args
+    }
+    function lh {
+        eza $ic $la -h $args
+    }
+    function lg {
+        eza $ic $la --group-directories-first $args
+    }
+    function lt {
+        eza $ic $tr --level=2 $args
+    }
+    function lt1 {
+        eza $ic $tr --level=1 $args
+    }
+    function lt2 {
+        eza $ic $tr --level=2 $args
+    }
+    function lt3 {
+        eza $ic $tr --level=3 $args
+    }
+    function lt4 {
+        eza $ic $tr --level=4 $args
+    }
+    function l {
+        eza $ic --no-filesize --no-time --no-user --no-permissions --long $tr --level=1 $args
     }
 }
 else {
