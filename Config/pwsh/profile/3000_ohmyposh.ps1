0e83b6c85243538daae1db063e75822f7a576d02
Juliano Marcon (CTO) <juliano.marcon@solagora.com.br>
2024-11-22 10:05:53 -0300

    feat: Add Set-PoshPrompt function to customize Oh-My-Posh theme initialization

--- Config/pwsh/profile/3000_ohmyposh.ps1
+++ Config/pwsh/profile/3000_ohmyposh.ps1
@@ -4,4 +4,15 @@ if ($ENV:PROFILE_DEBUG -eq $true) {
     Write-Host 'Setting Oh-My-Posh'
 }
 
-oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/atomic.omp.json | Invoke-Expression
+function Set-PoshPrompt {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$Theme
+    )
+
+    oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/$Theme.omp.json | Invoke-Expression
+}
+
+Set-PoshPrompt "atomic"
+
+# oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/atomic.omp.json | Invoke-Expression
