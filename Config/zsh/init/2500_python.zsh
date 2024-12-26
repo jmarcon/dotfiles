46f0f8109bc4b7b5c990ed8b1480d2a9993b85cf
Juliano Marcon <juliano.marcon@solagora.com.br>
2024-12-26 12:30:32 -0300

    Add uv and ruff

--- Config/zsh/init/2500_python.zsh
+++ Config/zsh/init/2500_python.zsh
@@ -7,3 +7,11 @@ if command -v pyenv >/dev/null 2>&1; then
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 fi
+
+if command -v ruff >/dev/null 2>&1; then 
+  eval "$(ruff generate-shell-completion zsh)"
+fi
+
+if command -v uv >/dev/null 2>&1; then 
+  eval "$(uv generate-shell-completion zsh)"
+fi
