0b945780d4cafab2a90499a8dfe9e098dfeaec76
Juliano Marcon (CTO) <juliano.marcon@solagora.com.br>
2025-01-17 08:32:14 -0300

    Create a function to change oh-my-posh theme

--- Config/pwsh/profile/3000_ohmyposh.ps1
+++ Config/pwsh/profile/3000_ohmyposh.ps1
@@ -7,10 +7,30 @@ if ($ENV:PROFILE_DEBUG -eq $true) {
 function Set-PoshPrompt {
     Param(
         [Parameter(Mandatory = $true)]
-        [string]$Theme
+        [string]$Theme = "atomic"
     )
 
     oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/$Theme.omp.json | Invoke-Expression
+}
+
+function Set-PoshTheme {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$themeReason = "dev"
+    )
+
+    if ($themeReason -eq "dev") {
+        Set-PoshPrompt "tokyonight_storm"
+    }
+    elseif ($themeReason -eq "cloud") {
+        Set-PoshPrompt "cloud-context"
+    }
+    elseif ($themeReason -eq "work") {
+        Set-PoshPrompt "easy-term"
+    }
+    else {
+        Set-PoshPrompt "atomic"
+    }
 }
 
 Set-PoshPrompt "atomic"
