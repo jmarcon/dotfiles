49d841bf2c5ceda7f433a3bf86a5e1d003f3c5ff
Juliano Marcon <juliano.marcon@outlook.com>
2025-07-14 10:52:07 -0300

    feat: Adiciona aliases para ferramentas de inteligência artificial no Zsh

--- Config/zsh/aliases/4999_othes.zsh
+++ Config/zsh/aliases/4999_othes.zsh
@@ -18,3 +18,13 @@ fi
 if command -v nvim >/dev/null 2>&1; then
     alias vim="nvim"
 fi
+
+if command -v npx >/dev/null 2>&1; then
+    # Artifitial Intelligence Cli
+    ## Gemini
+    alias gai="npx --yes https://github.com/google-gemini/gemini-cli"
+    ## Claude-Code
+    alias cc="npx --yes @anthropic-ai/claude-code@latest"
+    ## Rulesync (Sync rules in multiple AI. The rules must be edited in .rulesync/rules)
+    alias rulesync="npx --yes rulesync@latest"
+fi
