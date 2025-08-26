d213617f9e04d3ce8253594bd3e81e85c3d87ad2
Juliano Marcon <juliano.marcon@solagora.com.br>
2025-08-26 08:48:37 -0300

    feat: Enhance module loading logic to skip PSColors in non-interactive terminals

--- Config/pwsh/profile/0200_modules.ps1
+++ Config/pwsh/profile/0200_modules.ps1
@@ -20,12 +20,26 @@ $modules_to_load = @(
     "Microsoft.PowerShell.ConsoleGuiTools"
 )
 
+if ($env:TERM -eq "dumb" -or [Environment]::GetCommandLineArgs() -contains "-NonInteractive" -or $Host.Name -eq "ConsoleHost") {
+    # Não carrega recursos que dependem de terminal interativo
+    $env:SKIP_PSCOLORS = $true
+}
+
 foreach ($module in $modules_to_load) {
     Try {
         if ($ENV:OSTYPE -and $ENV:OSTYPE.Contains("darwin") -eq $true) {
             if ($module.Contains("PSProfiler") -eq $true) { continue }
             if ($module.Contains("PSColors") -eq $true) { continue }
         }
+
+        if ($module.Contains("PSColors") -eq $true) {
+            if (-not $env:SKIP_PSCOLORS -and $Host.Name -eq "ConsoleHost") { 
+                Import-Module -Name "$module" -ErrorAction Stop
+            }
+            else {
+                continue
+            }
+        }   
 
         Import-Module -Name "$module" -ErrorAction Stop
     }
