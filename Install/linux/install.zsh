36aead966b7b4a005895e9f33ef4c4ce341b27de
Juliano Marcon <juliano.marcon@solagora.com.br>
2026-01-29 21:04:59 -0300

    fix(format): improve indentation and readability in install and apt scripts

--- Install/linux/install.zsh
+++ Install/linux/install.zsh
@@ -4,45 +4,52 @@
 dir=$(dirname $(realpath $0))
 os_file="$dir/os.zsh"
 if [ -f "$os_file" ]; then
-  source $os_file
+	source $os_file
 fi
 
 # Is it windows?
 if [ "$CURRENT_OS" = "windows" ]; then
-
+	Write-Output "Windows doesn't support zsh."
 fi
 
 # Is it macos?
 if [ "$CURRENT_OS" = "macos" ]; then
+	## Homebrew
+	brew_file="$dir/homebrew.zsh"
+	if [ -f "$brew_file" ]; then
+		source $brew_file
+	fi
 
+	## Git repos
+	git_repos_file="$dir/git_repos.zsh"
+	if [ -f "$git_repos_file" ]; then
+		source $git_repos_file
+	fi
 fi
 
 # Is it linux?
 if [ "$CURRENT_OS" = "linux" ]; then
-  ## Apt
-  apt_file="$dir/apt.zsh"
-  if [ -f "$apt_file" ]; then
-    source $apt_file
-  fi
+	## Apt
+	apt_file="$dir/apt.zsh"
+	if [ -f "$apt_file" ]; then
+		source $apt_file
+	fi
 
-  ## Snap
-  snap_file="$dir/snap.zsh"
-  if [ -f "$snap_file" ]; then
-    source $snap_file
-  fi
-fi
+	## Snap
+	snap_file="$dir/snap.zsh"
+	if [ -f "$snap_file" ]; then
+		source $snap_file
+	fi
 
-# Is it linux or macos?
-if [ "$CURRENT_OS" = "linux" ] || [ "$CURRENT_OS" = "macos" ]; then
-  ## Homebrew
-  brew_file="$dir/homebrew.zsh"
-  if [ -f "$brew_file" ]; then
-    source $brew_file
-  fi
+	## Homebrew
+	brew_file="$dir/homebrew.zsh"
+	if [ -f "$brew_file" ]; then
+		source $brew_file
+	fi
 
-  ## Git repos
-  git_repos_file="$dir/git_repos.zsh"
-  if [ -f "$git_repos_file" ]; then
-    source $git_repos_file
-  fi
+	## Git repos
+	git_repos_file="$dir/git_repos.zsh"
+	if [ -f "$git_repos_file" ]; then
+		source $git_repos_file
+	fi
 fi
