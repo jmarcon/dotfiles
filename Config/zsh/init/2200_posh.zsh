08f471bd368cc4fb68556d611090aedb436315a5
Juliano Marcon <juliano.marcon@solagora.com.br>
2026-01-29 21:49:48 -0300

    fix(format): correct schema declaration format in radar.omp.toml and improve indentation in 2200_posh.zsh

--- Config/zsh/init/2200_posh.zsh
+++ Config/zsh/init/2200_posh.zsh
@@ -3,29 +3,36 @@ print_debug '  ♾️️ Initializing [2200] - Oh-My-Posh' 'yellow'
 
 verify_commands oh-my-posh || return 1
 
-if verify_commands brew; then
-    eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/atomic.omp.json)"
+# if ! eval "$(oh-my-posh init zsh --config /Users/jm/.git_repos/dotfiles/Config/posh/radar.omp.toml)"; then
+# 	echo '  ❌ Failed to initialize Oh-My-Posh with radar theme'
+# 	print_error '  ❌ Failed to initialize Oh-My-Posh'
+# else
+# 	return 0
+# fi
 
-    if [[ "$DEBUG_DOTFILES" == "true" ]]; then
-        print -P '%F{magenta}  🍺 Using brew to get the oh-my-posh theme'
-    fi
-    eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/iterm2.omp.json)"
+if verify_commands brew; then
+	eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/atomic.omp.json)"
+
+	if [[ "$DEBUG_DOTFILES" == "true" ]]; then
+		print -P '%F{magenta}  🍺 Using brew to get the oh-my-posh theme'
+	fi
+	eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/iterm2.omp.json)"
 else
-    if [ -n "${POSH_THEMES_PATH+1}" ]; then
-        if [[ "$DEBUG_DOTFILES" == "true" ]]; then
-            print -P '%F{magenta}  Using the $POSH_THEMES_PATH to get the oh-my-posh theme'
-        fi
-        eval "$(oh-my-posh init zsh)"
-        POSH_THEME="$POSH_THEMES_PATH/atomic.omp.json"
-    elif [ -d "/home/vscode/.oh-my-posh/themes" ]; then
-        if [[ "$DEBUG_DOTFILES" == "true" ]]; then
-            print -P '%F{magenta}  Force to use the /home/vscode/.oh-my-posh/themes to get the oh-my-posh them'
-        fi
-        eval "$(oh-my-posh init zsh)"
-        POSH_THEME='/home/vscode/.oh-my-posh/themes/atomic.omp.json'
-    else
-        if [[ "$DEBUG_DOTFILES" == "true" ]]; then
-            print -P '%F{magenta}  No oh-my-posh theme found'
-        fi
-    fi
+	if [ -n "${POSH_THEMES_PATH+1}" ]; then
+		if [[ "$DEBUG_DOTFILES" == "true" ]]; then
+			print -P '%F{magenta}  Using the $POSH_THEMES_PATH to get the oh-my-posh theme'
+		fi
+		eval "$(oh-my-posh init zsh)"
+		POSH_THEME="$POSH_THEMES_PATH/atomic.omp.json"
+	elif [ -d "/home/vscode/.oh-my-posh/themes" ]; then
+		if [[ "$DEBUG_DOTFILES" == "true" ]]; then
+			print -P '%F{magenta}  Force to use the /home/vscode/.oh-my-posh/themes to get the oh-my-posh them'
+		fi
+		eval "$(oh-my-posh init zsh)"
+		POSH_THEME='/home/vscode/.oh-my-posh/themes/atomic.omp.json'
+	else
+		if [[ "$DEBUG_DOTFILES" == "true" ]]; then
+			print -P '%F{magenta}  No oh-my-posh theme found'
+		fi
+	fi
 fi
